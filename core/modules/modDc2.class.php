3c08ce7b79d6d2f98d656a10503113d784e9a5be
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2025-11-26 13:53:16 +0100

    Restore modDc2 class with copyright notice

--- core/modules/modDc2.class.php
+++ core/modules/modDc2.class.php
@@ -57,7 +57,7 @@ class modDc2 extends DolibarrModules
 		$this->description = "Module permettant remplir le formulaire DC2 depuis la proposition commerciale";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
 		$this->version = '1.0.1';
-		$this->url_last_version = 'https://moduleversion.lesmetiersdubatiment.fr/ver.php?m=dc2'
+		$this->url_last_version = 'https://moduleversion.lesmetiersdubatiment.fr/ver.php?m=dc2';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		// Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
@@ -214,6 +214,7 @@ class modDc2 extends DolibarrModules
 }
 
 ?>
+
 
 
 
